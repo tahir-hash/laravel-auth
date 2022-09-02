d05bdd26cceaa0300d11dcd04dad1bc1fd3abbdf
tahir-hash <mbayepro6@gmail.com>
2022-09-02 16:24:21 +0000

    redirect

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+require __DIR__.'/auth.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +16,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+   
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth','role:admin'])->group(function(){
+    Route::get('/private', function (){
+        return 'je suis un admin';
+    });
+});
+
+Route::middleware(['auth','role:client'])->group(function(){
+    Route::get('/public', function (){
+        return 'je suis un client';
+    });
+});
