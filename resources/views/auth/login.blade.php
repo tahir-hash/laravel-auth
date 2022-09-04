a6b81b3b81758695d0a56ab0c190e9e91a954c86
tahir-hash <mbayepro6@gmail.com>
2022-09-04 23:27:16 +0000

    image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <img src="image/basket.png" width="100px" alt="">
             </a>
         </x-slot>
 
@@ -39,7 +39,11 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('INSCRIRPTION') }}
+                </a>
+            </div>
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ require __DIR__.'/auth.php';
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
    
 });
 
